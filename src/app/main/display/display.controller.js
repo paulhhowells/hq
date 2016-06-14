6aec1a28cf34f7a97ad8b37a02ddcd3ccd5cd649
phowells <paul.howells@capgemini.com>
2016-06-14 11:17:52 +0100

    added widgets to display view

--- src/app/main/display/display.controller.js
+++ src/app/main/display/display.controller.js
@@ -13,17 +13,30 @@
     .module('app')
     .controller('DisplayController', DisplayController);
 
-  DisplayController.$inject = ['$scope', '$interval', 'user', 'settings'];
+  DisplayController.$inject = ['$scope', '$interval', 'widget', 'feed', 'user', 'settings'];
 
-  function DisplayController ($scope, $interval, user, settings) {
+  function DisplayController ($scope, $interval, widget, feed, user, settings) {
 
     // View Model.
     var display = this;
 
     display.title = 'Display';
+    display.widget = widget;
+    display.feed = feed;
     display.user = user;
     display.pod = settings.pod;
 
+    var filler = [
+      { display : 'filler' },
+      { display : 'filler' },
+      { display : 'filler' }
+    ];
+    var displayList = [].concat(widget.displayList.get(), filler);
+
+    display.getWidgets = function () {
+      console.log('getWidgets');
+      return displayList;
+    };
 
     $scope.$on('$routeChangeSuccess', function (event) {
       console.log('display $routeChangeSuccess');
