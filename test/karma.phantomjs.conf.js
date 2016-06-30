9f136d3444e5807f9e0cb1cef8d94f56360d0442
phowells <paul.howells@capgemini.com>
2016-06-30 21:46:13 +0100

    added angular-aria and config.route files to karma configuration

--- test/karma.phantomjs.conf.js
+++ test/karma.phantomjs.conf.js
@@ -18,10 +18,12 @@ module.exports = function(config) {
     files: [
       '../node_modules/jquery/dist/jquery.js',
       '../node_modules/angular/angular.js',
+      '../node_modules/angular-aria/angular-aria.js',
       '../node_modules/angular-route/angular-route.js',
       '../node_modules/angular-mocks/angular-mocks.js',
       '../src/app/app.module.js',
       '../src/app/app.config.js',
+      '../src/app/app.config.route.js',
       '../src/app/**/*.js'
     ],
 
