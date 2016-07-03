c68843cdc7374746c63a0c9041f61fb1b94cbce5
phowells <paul.howells@capgemini.com>
2016-07-03 15:02:15 +0100

    removed a mocked up service idea

--- src/app/services/feed.service.js
+++ src/app/services/feed.service.js
@@ -12,37 +12,6 @@
     .factory('feed', feedService);
 
   function feedService () {
-    var feed = {
-      one : {
-        name : 'A',
-        type : 'mono',
-        url : '',
-        selected : false
-      },
-      two : {
-        name : 'B',
-        type : 'mono',
-        url : '',
-        selected : false
-      },
-      three : {
-        name : 'C',
-        type : 'mono',
-        url : '',
-        selected : false
-      },
-      fish : {
-        name : 'D',
-        type : 'mono',
-        url : '',
-        selected : false
-      },
-      pig : {
-        name : 'E',
-        type : 'mono',
-        url : '',
-        selected : false
-      }
     };
 
     return feed;
